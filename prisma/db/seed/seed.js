d66ca0c854d511c3cbeb8659caf876bebe212b69
fabricio hein <fabricio@exago.com>
2023-09-06 11:36:29 -0300

    alterado

--- prisma/db/seed/seed.js
+++ prisma/db/seed/seed.js
@@ -2,21 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.upsert({
-    where: { email: "admin@teste.com" },
-    update: {},
-    create: {
-      email: "admin@reservei.com",
-      senha: "$2a$10$i3vF7YAgOOrsZ0QaGwNHru11Ztm7mHmUln1o88gNRSDRxmpQ3Bdpm", // senha admin1234
-    },
-  });
-
-  prisma.cliente.create({
-    data: {
-      name,
-      cliente
-    }
-  })
+  
 
 }
 
